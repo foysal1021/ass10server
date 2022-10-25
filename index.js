2d862f7fd5b0bd4a9a85ec1d625fc5a2d37b46b3
foysal1021 <foysala1021@gmail.com>
2022-10-26 01:05:12 +0600

    server done

--- index.js
+++ index.js
@@ -19,12 +19,6 @@ app.get('/course/:id',(req,res)=>{
   res.send(CourseID)
 })
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
